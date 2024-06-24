900d175135dd59b5a74fb13ebe8c373559a6b8b3
Kenth Saya-ang <gargarkenth93@gmail.com>
2024-06-24 14:49:51 +0000

    working file metadata parser

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+
+const multer = require('multer')
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage })
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  console.log('Uploading...');
+  console.log(req.body);
+  console.log(req.file);
+
+  const { originalname: name, mimetype: type, size } = req.file;
+
+  res.json({ name, type, size });
 });
 
 
